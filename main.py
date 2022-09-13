62af650e544181ca0f7c0de282bebbcab3e59d9f
Caio Emmanuel <caioesr@ibm.com>
2022-09-12 23:05:56 -0300

    changing logic to use file as input

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
-from processes.PrePro import PrePro
 from parsers.Parser import Parser
 
 if __name__ == "__main__":
     import sys
 
-    codes = sys.argv[1:]
-    for code in codes:
-        code = PrePro.pre_process(code)
-        print(Parser.run(code).evaluate())
+    file = sys.argv[1]
+    with open(file, "r") as f:
+        code = f.read()
+    print(Parser.run(code).evaluate())
