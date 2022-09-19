f0204143d0ac82e38e1f0b63400f2eed05944ee6
Caio Emmanuel <caioesr@ibm.com>
2022-09-19 15:49:34 -0300

    changing to accept only carbon files

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ if __name__ == "__main__":
     import sys
 
     file = sys.argv[1]
+    if file.split('.')[-1] != 'carbon':
+        raise Exception('You must pass a carbon file')
+
     with open(file, "r") as f:
         code = f.read()
     print(Parser.run(code).evaluate())
