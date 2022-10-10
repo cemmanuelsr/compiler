49d6c57ec8a41059246b2852d40cd61d11b4e6b9
Caio Emmanuel <caioesr@ibm.com>
2022-10-10 17:07:11 -0300

    removing useless function to clean spaces

--- processes/PrePro.py
+++ processes/PrePro.py
@@ -8,21 +8,6 @@ class PrePro:
         return re.sub(r"\s*//.*", "", code)
 
     @staticmethod
-    def _clean_spaces(code: str):
-        op_space_num = re.search(r"[+|\-|*|\\/]\s+\d", code)
-        while op_space_num:
-            code = code[:op_space_num.start() + 1] + code[op_space_num.end() - 1:]
-            op_space_num = re.search(r"[+|\-|*|\\/]\s+\d", code)
-
-        num_space_op = re.search(r"\d\s+[+|\-|*|\\/]", code)
-        while num_space_op:
-            code = code[:num_space_op.start() + 1] + code[num_space_op.end() - 1:]
-            num_space_op = re.search(r"\d\s+[+|\-|*|\\/]", code)
-
-        return code
-
-    @staticmethod
     def pre_process(code: str):
         code = PrePro._clean_comments(code)
-        code = PrePro._clean_spaces(code)
         return code
