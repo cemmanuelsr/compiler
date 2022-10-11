1f0eb1d626e17fbbc679af494ebdc8f639db982d
Caio Emmanuel <caioesr@ibm.com>
2022-10-11 13:55:14 -0300

    fixing PrePro tests and initiating to change Parser tests

--- tests/TestParser.py
+++ tests/TestParser.py
@@ -6,36 +6,16 @@ from parsers.Parser import Parser
 class TestParser(unittest.TestCase):
 
     def test_1(self):
-        code = "   100   +  100  -100+  1"
+        file = "assets/codes/test_1.carbon"
+        with open(file, "r") as f:
+            code = f.read()
         self.assertEqual(Parser.run(code).evaluate(), 101)
 
     def test_2(self):
-        code = "1 1"
-        self.assertRaises(Exception, Parser.run, code)
-
-    def test_3(self):
-        code = "(1+1)*3"
-        self.assertEqual(Parser.run(code).evaluate(), 6)
-
-    def test_4(self):
-        code = "++--++2"
-        self.assertEqual(Parser.run(code).evaluate(), 2)
-
-    def test_5(self):
-        code = "(1+1)*(2+2)"
-        self.assertEqual(Parser.run(code).evaluate(), 8)
-
-    def test_6(self):
-        code = "(((1+1)))"
-        self.assertEqual(Parser.run(code).evaluate(), 2)
-
-    def test_7(self):
-        code = "40--2"
-        self.assertEqual(Parser.run(code).evaluate(), 42)
-
-    def test_8(self):
-        code = "40+-+-2"
-        self.assertEqual(Parser.run(code).evaluate(), 42)
+        file = "assets/codes/test_2.carbon"
+        with open(file, "r") as f:
+            code = f.read()
+        self.assertEqual(Parser.run(code).evaluate(), 101)
 
 
 if __name__ == "__main__":
--- tests/TestPrePro.py
+++ tests/TestPrePro.py
@@ -5,19 +5,9 @@ from processes.PrePro import PrePro
 
 class TestPrePro(unittest.TestCase):
 
-    def test_clean_comments(self):
+    def test_pre_process(self):
         code = "1 + 2*3  //some comment here"
         cleaned_code = "1 + 2*3"
-        self.assertEqual(cleaned_code, PrePro._clean_comments(code))
-
-    def test_clean_spaces(self):
-        code = "1    + 2  *3"
-        cleaned_code = "1+2*3"
-        self.assertEqual(cleaned_code, PrePro._clean_spaces(code))
-
-    def test_pre_process(self):
-        code = "1    + 2  *3//some comment here"
-        cleaned_code = "1+2*3"
         self.assertEqual(cleaned_code, PrePro.pre_process(code))
 
 
