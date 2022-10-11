58823aad042ffc986a53315db55869db5d35f7ac
Caio Emmanuel <caioesr@ibm.com>
2022-10-11 16:12:32 -0300

    draw script created

---
+++ draw.py
@@ -0,0 +1,11 @@
+import subprocess
+
+if __name__ == "__main__":
+    import sys
+
+    file = sys.argv[1]
+    filename = file.split('/')[-1].split('.')[:-1][0]
+    if file.split('.')[-1] != 'dot':
+        raise Exception('You must pass a dot file')
+
+    subprocess.call(["dot","-Tsvg",file,"-o",f"{filename.svg}"])
